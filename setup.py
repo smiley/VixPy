61068050297a5b0bedb7715e8bdc98297256b619
smiley <smiley@users.noreply.github.com>
2017-05-28 17:39:10 +0300

    Created a "setup.py" file for easy installation.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='vixpy',
+      version='1.0',
+      description='A Pythonic implementation of the VMware VIX API.',
+      author='@yzlin',
+      packages=['vixpy'],
+      install_requires=[]
+      )
